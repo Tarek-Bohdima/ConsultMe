9a724cae66b684e0a68ac634120059978e19ddc0
TarekBohdima <Tarek.buhdeima@gmail.com>
2025-08-02 00:48:27 +0300

    Refactor: Update Kotlin compiler options in feature-chat module
    This commit updates the Kotlin compiler options in the `feature-chat` module.
    The `jvmTarget` is now set using `org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17` and `freeCompilerArgs` is set to `listOf("-Xcontext-receivers")` within the `kotlin` block.
    The previous `kotlinOptions` block within the `android` block has been removed.

--- feature-chat/build.gradle.kts
+++ feature-chat/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 
 kotlin {
     jvmToolchain(17)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        freeCompilerArgs.set(listOf("-Xcontext-receivers"))
+    }
 }
 
 android {
@@ -31,10 +35,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs = listOf("-Xcontext-receivers")
     }
 
     lint {
